f53c86c190ecbcb585e845cbe2d6a0f6da6dec79
PotatoScript <potatoscript@yahoo.com>
2024-05-25 21:43:03 +0900

    Update data_read.php

--- docs/data_read.php
+++ docs/data_read.php
@@ -1,16 +1,24 @@
 <?php
 
-$data = [
-    [1, "potato", 5, "1000", ""],
-    [3, "🍊", 2, "500", ""],
-    [9, "🥔", 10, "0", ""],
-    [10, "", 0, "0", ""],
-    [11, "", 0, "0", ""],
-    [18, "abc", 100, "0", ""],
-    [21, "", 0, "0", ""],
-    [22, "", 0, "0", ""]
-];
-
-echo $data;
+require_once("dbconfig.php");
+ 
+$datalist = array();
+ 
+$query = "SELECT id,name,qty,price FROM potato.potato order by id  ";
+$result = $pdo->query($query);
+ 
+if($result){
+  while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+      $datalist[] = array(
+        $row['id'],
+        $row['name'],
+        $row['qty'],
+        $row['price'],
+        "<input type='button' value='削除' class='delete_button' />"
+      );
+  }
+}
+ 
+echo json_encode($datalist);
 
 ?>
